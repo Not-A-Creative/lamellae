8f8c9f4ab13edb613a114d57fea12d0ba5862060
Not_A_Creative <not_a_creative@outlook.com>
2024-09-24 17:20:43 +0100

    Added initial version for comb visualisation

--- lamellae.lua
+++ lamellae.lua
@@ -16,12 +16,16 @@ DRUM_DISPLAY_START_X = 28
 
 NOTE_DISPLAY_SIZE = 2
 
+KEY_BASE_THICKNESS = NOTE_DISPLAY_SIZE
+
 
 key1_held = false
 
 is_motor_running = false
 
 drum = {}
+
+key_sprites = {}
 
 scale_names = {}
 for i = 1,#MusicUtil.SCALES do
@@ -37,7 +41,7 @@ function init()
   screen.level(15)
   
   -- PARAMS SETUP
-  params:add{type = "number", id = "num_of_keys", name = "Number of Keys", min = 5, max = 18, default = 18, action = function() generate_drum(params:get("num_of_notes")) end}
+  params:add{type = "number", id = "num_of_keys", name = "Number of Keys", min = 5, max = 18, default = 18, action = function() generate_drum(params:get("num_of_notes")); create_key_sprites() end}
   params:add{type = "number", id = "drum_length", name = "Drum Length", min = 1, max = 10, default = 2, action = function() generate_drum(params:get("num_of_notes")) end}
   params:add{type = "number", id = "num_of_notes", name = "Number of Notes", min = 10, max = 200, default = 50, action = function() generate_drum(params:get("num_of_notes")) end}
   params:add{type = "trigger", id = "regen", name = "Regenerate Drum", action = function() generate_drum(params:get("num_of_notes")) end}
@@ -70,27 +74,35 @@ end
 function redraw()
   screen.clear()
   
-  -- PLACEHOLDER FOR COMB
   screen.line_width(1)
-  screen.move(DRUM_DISPLAY_START_X, 0)
-  screen.line(DRUM_DISPLAY_START_X, 64)
-  screen.stroke()
+  
+  -- PLACEHOLDER FOR COMB
+  --screen.line_width(1)
+  --screen.move(DRUM_DISPLAY_START_X, 0)
+  --screen.line(DRUM_DISPLAY_START_X, 64)
+  --screen.stroke()
   -- TODO: Comb graphics and animation
+  for _,key in ipairs(key_sprites) do
+    screen.move(0, key.base_top_y)
+    screen.line(key.x, key.end_top_y)
+    screen.line(key.x, key.end_bottom_y)
+    screen.line(0, key.base_top_y)
+  end
   
   -- draw notes
   for _,note in ipairs(drum) do
     local x = note.x
-    local y = math.floor(((KEY_DISPLAY_HEIGHT / params:get("num_of_keys")) * note.key) + KEY_DISPLAY_PADDING)
+    local y = calculate_key_y_coord(note.key)
     
     screen.rect(x, y, NOTE_DISPLAY_SIZE, NOTE_DISPLAY_SIZE)
-    screen.fill()
   end
+  screen.fill()
   screen.update()
 end
 
 
 function enc(n,d)
-  -- TODO ENC2 for 'motor' speed
+  
   if n == 2 then
     params:delta("motor_time", d)
   end
@@ -122,7 +134,7 @@ end
 
 
 function play_note(note)
-  if note.x == DRUM_DISPLAY_START_X + 2 then
+  if note.x == DRUM_DISPLAY_START_X + NOTE_DISPLAY_SIZE then
     engine.hz(key_freq[note.key])
   end
 end
@@ -145,15 +157,31 @@ end
 
 function run_motor()
   if is_motor_running then
-      motor.time = params:get("motor_time")
-      motor:start()
-    else
-      motor:stop()
+    motor.time = params:get("motor_time")
+    motor:start()
+  else
+    motor:stop()
   end
 end
 
 function motor_tick()
   enc(3,1) -- turns ENC3 once
+end
+
+
+function create_key_sprites()
+  key_sprites = {} -- Same issue as notes!
+  
+  for i = 1,params:get("num_of_keys") do
+    local y = calculate_key_y_coord(i)
+    local coords = {key = i, x = DRUM_DISPLAY_START_X, base_top_y = y, base_bottom_y = (y + KEY_BASE_THICKNESS), end_top_y = y, end_bottom_y = (y + KEY_BASE_THICKNESS)}
+    table.insert(key_sprites, coords)
+  end
+end
+
+
+function calculate_key_y_coord(key)
+  return math.floor(((KEY_DISPLAY_HEIGHT / params:get("num_of_keys")) * key) + KEY_DISPLAY_PADDING)
 end
 
 function build_scale()
