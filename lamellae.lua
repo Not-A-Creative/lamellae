cb11f1d12ac7f83c40d53b486bcaff4231ed1699
Not-A-Creative <not_a_creative@outlook.com>
2024-11-17 22:38:52 +0000

    v1.1.0
    Added MIDI out and option to turn off engine

--- lamellae.lua
+++ lamellae.lua
@@ -10,7 +10,7 @@
 -- Note, scale and engine
 -- options in params
 --
--- v1.0.0 @Not_A_Creative
+-- v1.1.0 @Not_A_Creative
 
 MusicUtil = require("lib/musicutil")
 
@@ -32,6 +32,8 @@ PLATE_END_X = PATTERN_DISPLAY_START_X + 1.75
 PLATE_DISPLAY_LEVEL_DEFAULT = 9
 PLATE_BASE_THICKNESS = NOTE_DISPLAY_SIZE
 
+DEFAULT_MIDI_VELOCITY = 97
+
 
 -- VARIABLES
 
@@ -48,6 +50,19 @@ for i = 1,#MusicUtil.SCALES do
   table.insert(scale_names, MusicUtil.SCALES[i].name)
 end
 
+-- Midi
+midi_devices = {}
+midi_device_names = {}
+midi_channel = 1
+
+active_midi_notes = {}
+
+-- credit to tehn's awake script for this implementation of midi device naming
+for i = 1,#midi.vports do
+  midi_devices[i] = midi.connect(i)
+  table.insert(midi_device_names, "Port " .. i .. " " .. (#midi_devices[i].name < 15 and midi_devices[i].name or util.acronym(midi_devices[i].name)))
+end
+
 
 function init()
   screen.aa(1)
@@ -55,6 +70,11 @@ function init()
   screen.level(15)
   
   -- PARAMS SETUP
+  params:add_group("midi_settting", "MIDI", 3)
+  params:add{type = "option", id = "use_midi", name = "Use Midi", options = {"No", "Yes"}, default = 1, action = show_midi_param_options}
+  params:add{type = "option", id = "midi_device_in_use", name = "Midi Device", options = midi_device_names, action = function(x) midi_channel = x; midi_device = midi_devices[x] end}
+  params:add{type = "control", id = "midi_note_length", name = "Midi Note Length", controlspec = controlspec.def{min = 0.1, max = 5.0, default = 1, step = 0.1, warp = "lin", units = "s"}}
+  
   params:add{type = "control", id = "auto_run_time", name = "Auto Play Speed", controlspec = controlspec.def{min = 0.5, max = 20, default = 5, step = 0.5, quantum = (1 / (2*19.5)), warp = "lin"}, action = function() set_auto_run_time() end}
   
   params:add_separator("scale_params", "Lamellae & Scale")
@@ -68,6 +88,7 @@ function init()
   params:add{type = "trigger", id = "regen", name = "Regenerate Pattern", action = function() generate_pattern(params:get("num_of_notes")) end}
 
   params:add_separator("engine_controls", "Engine")
+  params:add{type = "option", id = "use_engine", name = "Use Engine", options = {"No", "Yes"}, default = 2, action = show_engine_option_params}
   params:add{type = "control", id = "amp", name = "Amp", controlspec = controlspec.def{min = 0, max = 1, warp = "lin", step = 0.1, default = 0.8, quantum = 0.1}, action = function(x) engine.amp(x) end}
   params:add{type = "control", id = "cutoff", name = "Filter Cutoff", controlspec = controlspec.def{min = 20, max = 20000, warp = "exp", default = 800, step = 10, units = "hz", wrap = false}, action = function(x) engine.cutoff(x) end}
   params:add{type = "control", id = "pan", name = "Pan", controlspec = controlspec.PAN, action = function(x) engine.pan(x) end}
@@ -80,6 +101,8 @@ function init()
   screen_refresh:start(1/SCREEN_REFRESH_RATE)
   
   auto_run_metro = metro.init(auto_run_tick)
+  
+  midi_note_time_out = metro.init(all_midi_notes_off)
   
   params:bang()
 end
@@ -143,7 +166,26 @@ end
 
 function play_note(note)
   if note.x == PATTERN_DISPLAY_START_X + NOTE_DISPLAY_SIZE then
-    engine.hz(plate_freq[note.plate])
+    
+    if params:string("use_engine") == "Yes" then
+      engine.hz(plate_freq[note.plate])
+    end
+  
+    if params:string("use_midi") == "Yes" then
+      local note_num = plate_nums[note.plate]
+      
+      table.insert(active_midi_notes, note_num)
+      midi_device:note_on(note_num, DEFAULT_MIDI_VELOCITY, midi_channel)
+      midi_note_time_out:start(params:get("midi_note_length"))
+    end
+  
+  end
+end
+
+
+function all_midi_notes_off()
+  for _,note in ipairs(active_midi_notes) do
+    midi_device:note_off(note, DEFAULT_VELOCITY, midi_channel)
   end
 end
 
@@ -234,8 +276,42 @@ function update_num_of_plates()
 end
 
 
+function show_midi_param_options()
+  local midi_param_ids = {"midi_device_in_use", "midi_note_length"}
+  
+  for _,id in ipairs(midi_param_ids) do
+    if params:string("use_midi") == "Yes" then
+      params:show(id)
+    else
+      params:hide(id)
+    end
+  end
+  
+  _menu.rebuild_params()
+end
+
+
+function show_engine_option_params()
+  local engine_param_ids = {"amp", "cutoff", "pan", "pw", "release"}
+  
+  for _,id in ipairs(engine_param_ids) do
+    if params:string("use_engine") == "Yes" then
+      params:show(id)
+    else
+      params:hide(id)
+    end
+  end
+  _menu.rebuild_params()
+end
+
+
 function refresh()
   if screen_dirty then
     redraw()
   end
+end
+
+
+function cleanup()
+  all_midi_notes_off()
 end
